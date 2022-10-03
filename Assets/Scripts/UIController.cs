d6396088c91e53587ea285472e155b627becc4d3
Aleksandr <aleksandr.v.rybakov@gmail.com>
2022-10-03 17:59:55 +0300

    work_commit_20221003

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Text levelNameText;
+
+    private void Awake()
+    {
+        levelNameText.text = SceneManager.GetActiveScene().name.ToString();
+    }
+}
